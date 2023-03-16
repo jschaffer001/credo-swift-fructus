4597f8207f9280241e6878b43b9e72e96b94d7ef
Jonathan <7716662+jschaffer001@users.noreply.github.com>
2023-03-16 16:47:55 -0700

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Jonathan Schaffer on 3/16/23.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
