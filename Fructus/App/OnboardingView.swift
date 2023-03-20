afd4dacb54bd89c51e9f3890d1371d23d506ad74
Jonathan <7716662+jschaffer001@users.noreply.github.com>
2023-03-20 13:53:44 -0700

    Updated the Onboarding view to incorporate the fruits data model so that each tab sequentially displays a different fruit as defined by the FruitData file.

--- Fructus/App/OnboardingView.swift
+++ Fructus/App/OnboardingView.swift
@@ -11,12 +11,13 @@ struct OnboardingView: View {
     // MARK: - PROPERTIES
     
     
+    var fruits: [Fruit] = fruitsData
+    
     // MARK: - BODY
     var body: some View {
         TabView {
-            ForEach(0..<5) { item in
-                // FruitCardView()
-                Text("Cards")
+            ForEach(fruits[0...5]) { item in
+                FruitCardView(fruit: item)
             } //: LOOP
         } //: TAB
         .tabViewStyle(PageTabViewStyle())
@@ -29,6 +30,6 @@ struct OnboardingView: View {
 
 struct OnboardingView_Previews: PreviewProvider {
     static var previews: some View {
-        OnboardingView()
+        OnboardingView(fruits: fruitsData)
     }
 }
